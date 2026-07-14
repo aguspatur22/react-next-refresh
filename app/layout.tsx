760fb42c7e501f106f76f0834053cb81c34829eb
Agustin Paturlanne <aguspaturlanne@gmail.com>
2026-07-14 13:38:40 -0300

    feat: added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | React Next Refresh',
+    default: 'React Next Refresh',
+  },
+  description: 'React Next Refresh course',
+};
 
 export default function RootLayout({
   children,
